9013f75e91df8f83d79b29ee6fda6b8d3255bfca
Alex <alexboyev89@gmail.com>
2026-01-21 22:33:25 +0200

    jenkins update

--- aws-infrastrucutre-terraform/modules/ec2/bootstrap.sh
+++ aws-infrastrucutre-terraform/modules/ec2/bootstrap.sh
@@ -84,7 +84,7 @@ fi
 
 log "Installing Jenkins..."
 if ! need_cmd jenkins; then
-  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc >/dev/null
+  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc >/dev/null
   echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list >/dev/null
   sudo apt-get update -y
   sudo apt-get install -y jenkins
